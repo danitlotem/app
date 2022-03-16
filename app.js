4f1ca856a87d666222ade93cbeb010bf6318a9aa
Mor Zemach <90982428+MorZemach@users.noreply.github.com>
2022-03-16 17:31:28 +0200

    app file
    updated urls according to routes names files

--- app.js
+++ app.js
@@ -14,15 +14,15 @@ app.use(bodyParser.json());
 
 
 //routes
-const usersRoutes = require('./api/routes/users_route');
-const userConfigurationRoutes=require('./api/routes/userConfiguration_route');
-const hobbiesRoutes = require('./api/routes/hobbies_route');
-const userPicturesRoutes = require('./api/routes/userPictures_route');
-const authRoutes = require('./api/routes/auth_route');
-const chatsRoutes = require("./api/routes/chats_route");
-const filtersRoutes = require("./api/routes/filters_route");
-const connectionsRoutes = require("./api/routes/connections_route");
-const notificationsRoutes = require("./api/routes/notifications_route");
+const usersRoutes = require('./api/routes/users');
+const userConfigurationRoutes=require('./api/routes/userConfiguration');
+const hobbiesRoutes = require('./api/routes/hobbies');
+const userPicturesRoutes = require('./api/routes/userPictures');
+const authRoutes = require('./api/routes/auth');
+const chatsRoutes = require("./api/routes/chats");
+const filtersRoutes = require("./api/routes/filters");
+const connectionsRoutes = require("./api/routes/connections");
+const notificationsRoutes = require("./api/routes/notifications");
 
 app.use('/users',usersRoutes);
 app.use('/userConfiguration',userConfigurationRoutes);
