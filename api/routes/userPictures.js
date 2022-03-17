324bb0c028e3f3fc49022cc217acb6f89a2f4db5
shellysp94 <59768741+shellysp94@users.noreply.github.com>
2022-03-17 22:26:14 +0200

    Add files via upload
    change users and users configuration tables

--- api/routes/userPictures.js
+++ api/routes/userPictures.js
@@ -3,14 +3,17 @@ const router = express.Router();
 
 const {
     getUserPictures, 
-    createUserPicture,
     deleteUserPicture,
-    updateUserPicture
+    updateUserPicture,
+    storeUserPic,
+    getUserMainPicture,
+    upload
 } = require('../controllers/userPictures');
 
 router.get('/:userid',getUserPictures); //get user by appid (/hobbies/:userid)
-router.post('/:userid', createUserPicture); //post (/hobbies/)
-router.delete('/:userid', deleteUserPicture) //delete user (/hobbies/:userid)
+router.get('/main/:userid', getUserMainPicture);
+router.post('/:userid', upload, storeUserPic);
+router.delete('/:userid', deleteUserPicture) //delete user specific picture
 router.put('/:userid', updateUserPicture); //update user
 
 module.exports=router;
