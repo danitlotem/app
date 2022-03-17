08379420b3ee3631f3b485239b937c11c8fd835f
shellysp94 <59768741+shellysp94@users.noreply.github.com>
2022-03-17 22:24:35 +0200

    Add files via upload
    update users and users configuration columns

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,80 +1,86 @@
-const { app, jwt } = require("../../app");
-const dbConfig = require("../../config/db_config");
+const { app, jwt } = require('../../app');
+const dbConfig = require('../../config/db_config');
 const mySqlConnection = dbConfig;
-const publicToken = require("../../config/auth_config");
+const publicToken= require('../../config/auth_config');
 const bcrypt = require("bcrypt");
 
-module.exports = {
-  login: async (req, res) => {
-    // Read username and password from request body
-    const email = req.body.email;
-    const password = req.body.password;
+module.exports={
+    
+    login: async (req,res) => {
+        // Read username and password from request body
+        const email=req.body.email;
+        const password=req.body.password;
 
-    mySqlConnection.query(
-      "SELECT* from users WHERE email=?",
-      [email],
-      (err, rows) => {
-        if (rows.length === 0) {
-          // user not exist
-          return res.send("user not exist");
-        } else {
-          //user exist
-          var resultArray = Object.values(JSON.parse(JSON.stringify(rows)));
-          var dbPass = resultArray[0].password;
-
-          bcrypt.compare(password, dbPass, (err, result) => {
-            if (err) {
-              return res.send(err);
+        mySqlConnection.query("SELECT* from users WHERE email=?", [email], (err, rows) => {
+            if (rows.length===0)   // user not exist
+            {
+                return res.send("user not exist");
             }
 
-            if (result) {
-              const accessToken = jwt.sign(
-                { user_id: resultArray[0].user_id },
-                publicToken,
-                { expiresIn: "3d" }
-              );
+            else 
+            { //user exist
+                var resultArray = Object.values(JSON.parse(JSON.stringify(rows)))
+                var dbPass = resultArray[0].password;
 
-              const userCred = {
-                user_id: resultArray[0].user_id,
-                email: email,
-                token: accessToken,
-              };
-              return res.send(userCred);
-            } else {
-              return res.send("Incorrect password");
+                bcrypt.compare(password, dbPass, (err, result) => 
+                {
+                    if (err)
+                    {
+                       return res.send(err);
+                    }
+
+                    if(result)
+                    {
+                        const accessToken = jwt.sign(
+                            { user_id: resultArray[0].user_id},
+                            publicToken,
+                            {expiresIn: '3d'}
+                        );
+
+                        const userCred = 
+                        {
+                            user_id : resultArray[0].user_id,
+                            email : email,
+                            token : accessToken
+                        }
+                        return res.send(userCred);
+                    }
+
+                    else
+                    {
+                        return res.send("Incorrect password");
+                    }
+                })
             }
-          });
-        }
-      }
-    );
-  },
-
-  register: (req, res) => {
-    mySqlConnection.query(
-      "SELECT* from users WHERE email=?",
-      [req.body.email],
-      async (err, rows) => {
-        if (err) {
-          console.log(err);
-        }
-
-        if (rows.length > 0) {
-          return res.send("This email already in use");
-        }
-
-        let hashedPassword = await bcrypt.hash(req.body.password, 10);
-        mySqlConnection.query(
-          "INSERT INTO users SET ?",
-          { email: req.body.email, password: hashedPassword },
-          (err, results) => {
-            if (err) {
-              console.log(err);
-            } else {
-              return res.send("User added");
+        });
+    },
+    
+    register: (req,res) => 
+    {
+        mySqlConnection.query("SELECT* from users WHERE email=?",[req.body.email], async (err,rows)=>
+        {   
+            if(err)
+            {
+                console.log(err);
             }
-          }
-        );
-      }
-    );
-  },
-};
+
+            if(rows.length>0)
+            {
+                return res.send("This email already in use");
+            }
+
+            let hashedPassword = await bcrypt.hash(req.body.password,10);
+            mySqlConnection.query('INSERT INTO users SET ?',{email:req.body.email, password: hashedPassword}, (err, results) =>
+            {
+                if (err)
+                {
+                    console.log(err);
+                }
+                else
+                {
+                    return res.send("User added");
+                }
+            });
+        })
+    }   
+}
